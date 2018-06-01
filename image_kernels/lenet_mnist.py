e93e61592ed054c00b8afb9e1b43cbdb95d4cd39
mstczuo <mstczuo@163.com>
2018-06-01 13:19:11 +0800

    add queue runner

--- image_kernels/lenet_mnist.py
+++ image_kernels/lenet_mnist.py
@@ -15,7 +15,7 @@ def build_train_op(image_tensor, label_tensor):
     lenet_argscope = lenet_arg_scope(weight_decay=FLAGS.weight_decay) 
     with slim.arg_scope(lenet_argscope):
         logits, end_points = lenet(image_tensor, is_training=True)
-    loss = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=label_tensor)
+    loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=label_tensor))
     end_points['loss'] = loss
     optimizer = tf.train.AdadeltaOptimizer(learning_rate=FLAGS.learning_rate)
     train_op = optimizer.minimize(loss)
@@ -24,12 +24,16 @@ def build_train_op(image_tensor, label_tensor):
 def train(train_op, end_points):
     sess = tf.InteractiveSession()
     tf.global_variables_initializer().run()
+    coord = tf.train.Coordinator()
+    threads = tf.train.start_queue_runners(coord=coord)
     for step in range(FLAGS.num_iters):
         if (step + 1) % 100 == 0:
             loss_val = sess.run([train_op, end_points['loss']])
             print('loss:' + str(loss_val))
         else:
             train_op.run()
+    coord.request_stop()
+    coord.join(threads)
 
 def main(_):
     image_tensor, label_tensor = load_mnist('train')
